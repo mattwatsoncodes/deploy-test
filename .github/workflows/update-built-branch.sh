c6466869890e7d690faafb995f33e0672a98f765
Matt Watson <mattwatson@Matts-MacBook-Pro.local>
2024-02-10 22:41:02 +0000

    Debug rsync

--- .github/workflows/update-built-branch.sh
+++ .github/workflows/update-built-branch.sh
@@ -65,7 +65,8 @@ cd ${BUILD_DIR}/wp-content
 #    rsync -rlIpz --info=progress2 --temp-dir=~/tmp --delay-updates --ipv4 --exclude=.git -e 'ssh -o "StrictHostKeyChecking=no" -p 2222' './${i}' '${DEPLOY_BRANCH}.${PANTHEON_PROJECT_ID}@appserver.${DEPLOY_BRANCH}.${PANTHEON_PROJECT_ID}.drush.in:code/wp-content/${FOLDER}'
 # done
 
-rsync -rlIpz --info=progress2 --temp-dir=~/tmp --delay-updates --ipv4 --exclude=.git -e 'ssh -o "StrictHostKeyChecking=no" -p 2222' './themes/my-theme' '$DEPLOY_BRANCH.$PANTHEON_PROJECT_ID@appserver.$DEPLOY_BRANCH.$PANTHEON_PROJECT_ID.drush.in:code/wp-content/themes'
+RSYNC= "-rlIpz --info=progress2 --temp-dir=~/tmp --delay-updates --ipv4 --exclude=.git -e 'ssh -o \"StrictHostKeyChecking=no\" -p 2222' './themes/my-theme' '${DEPLOY_BRANCH}.${PANTHEON_PROJECT_ID}@appserver.${DEPLOY_BRANCH}.${PANTHEON_PROJECT_ID}.drush.in:code/wp-content/themes'"
+rsync "$RSYNC"
 # rsync -rlIpz --info=progress2 --temp-dir=~/tmp --delay-updates --ipv4 --exclude=.git -e 'ssh -o "StrictHostKeyChecking=no" -p 2222' './themes/my-theme' 'dev.65401411-429a-476e-8dc8-4bd98e5d9164@appserver.dev.65401411-429a-476e-8dc8-4bd98e5d9164.drush.in:code/wp-content/themes'
 # mkdir -p ~/.ssh/ && touch ~/.ssh/known_hosts
 # echo "[appserver.dev.${PANTHEON_PROJECT_ID}.drush.in]:2222" > ~/.ssh/known_hosts
